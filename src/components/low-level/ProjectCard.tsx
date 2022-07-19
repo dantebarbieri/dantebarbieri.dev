e4291892775b63b1421e7ab7b0dd032327509299
pulchroxloom <code@dantebarbieri.dev>
2022-07-19 11:34:51 -0500

    Fix spacing

--- src/components/low-level/ProjectCard.tsx
+++ src/components/low-level/ProjectCard.tsx
@@ -29,57 +29,57 @@ const ProjectCard = (props: Props) => {
   const tryReplaceTechnology = (technology: string) => {
     let children: React.ReactElement<any, any> | undefined
     switch (technology) {
-      case 'C++': children = <CppIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'C++': children = <CppIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Kotlin': children = <KotlinIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Kotlin': children = <KotlinIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Java': children = <JavaIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Java': children = <JavaIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Javascript': children = <JavascriptIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Javascript': children = <JavascriptIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Typescript': children = <TypescriptIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Typescript': children = <TypescriptIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'PostgreSQL': children = <PostgresqlIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'PostgreSQL': children = <PostgresqlIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'LaTeX': children = <LatexIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'LaTeX': children = <LatexIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Ruby': children = <RubyIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Ruby': children = <RubyIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Rust': children = <RustIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Rust': children = <RustIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'HTML': children = <HtmlIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'HTML': children = <HtmlIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'CSS': children = <CssIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'CSS': children = <CssIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Shell': children = <ShellIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Shell': children = <ShellIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'React': children = <ReactIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'React': children = <ReactIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Processing': children = <ProcessingIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Processing': children = <ProcessingIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Azure': children = <AzureIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Azure': children = <AzureIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'AWS': children = <AwsIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'AWS': children = <AwsIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Firebase': children = <FirebaseIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Firebase': children = <FirebaseIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Git': children = <GitIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Git': children = <GitIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Python': children = <PythonIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Python': children = <PythonIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'MATLAB': children = <MatlabIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'MATLAB': children = <MatlabIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Lua': children = <LuaIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Lua': children = <LuaIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Jupyter': children = <JupyterIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Jupyter': children = <JupyterIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Apple': children = <AppleIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Apple': children = <AppleIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Android': children = <AndroidIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Android': children = <AndroidIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Windows': children = <WindowsIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Windows': children = <WindowsIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
-        case 'Linux': children = <LinuxIcon htmlColor={theme.palette.primary.dark} sx={{'&:hover': { fill: theme.palette.secondary.dark }}} />
+      case 'Linux': children = <LinuxIcon htmlColor={theme.palette.primary.dark} sx={{ '&:hover': { fill: theme.palette.secondary.dark } }} />
         break
     }
     if (children) {
@@ -91,7 +91,7 @@ const ProjectCard = (props: Props) => {
   }
 
   return (
-    <Card variant='outlined' sx={{ maxWidth: '30vw', minWidth: '20vw', m: 3.3 }}>
+    <Card variant='outlined' sx={{ maxWidth: '30vw', minWidth: '20vw', m: 3.3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
       <CardHeader title={title} titleTypographyProps={{ color: theme.palette.secondary.main, align: 'center', pb: 3, mt: 1.3, fontWeight: 'bold' }} subheader={description} subheaderTypographyProps={{ color: theme.palette.primary.main, align: 'center' }} action={repository ? <IconButton sx={{ position: 'absolute', top: theme.spacing(1), right: theme.spacing(1) }} size='small' href={repository}><GitHub htmlColor={theme.palette.primary.dark} fontSize="small" /></IconButton> : undefined} sx={{ position: 'relative' }} />
       <CardActions sx={{ justifyContent: 'center' }}>
         <Button variant='contained' href={href}><Typography variant='button'>Explore</Typography></Button>
